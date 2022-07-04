7b85613c8a73d331f94020a2b3c50c17386b773f
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-07-04 08:59:46 +0300

    Task 32 added

---
+++ Task_32/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Напишите программу замены элементов массива: положительные элементы
+замените на соответствующие отрицательные, и наоборот */
+
+int[] array = new int[10];
+
+for (int i = 0; i < array.Length; i++){
+    array[i] = new Random().Next(-20,21);
+    Console.Write(array[i] + " ");
+}
+for (int i = 0, i < array.Length; i++){
+    array[i] *= -1;
+    Console.Write(array[i] + " ");
+}
