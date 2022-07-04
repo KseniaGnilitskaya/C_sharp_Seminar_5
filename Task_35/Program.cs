e85be066d6fecf428f53a26963b00f9569b65539
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-07-04 09:00:22 +0300

    Task 35 added

---
+++ Task_35/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задайте одномерный массив из 123 случайных чисел.
+Найдите количество элементов массива, значения которых лежат
+в отрезке [10, 99]*/
+
+int[] array = new int[123];
+
+for (int i = 0; i < array.Length; i++) {
+    array[i] = new Random().Next(1, 200);
+    Console.Write(array[i] + " ");
+}
+
+int count = 0;
+
+for (int i = 0; i < array.Length; i++) {
+    if (array[i] >= 10 & array[i] <= 99) {
+        count += 1;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Number of items between 10 and 99 is {count}");
